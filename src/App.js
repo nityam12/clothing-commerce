e6e33ed418e46d637d35a6cf3128e695e4b3ea8a
nityam12 <nityam1050@gmail.com>
2020-06-15 13:07:03 -0700

    update our app with a shop image

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App(props) {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop/" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Shop_Data from "./shop.data.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props); // for access to state
+
+    this.state = {
+      collections: Shop_Data,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
