17017d47848020c4be4b740be838ce131680ebf5
nityam12 <nityam1050@gmail.com>
2020-07-09 02:14:04 -0700

    updated our code to pwa compatible,added service worker

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -14,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
@@ -32,18 +33,21 @@ app.listen(port, (error) => {
   console.log("Server running on port" + port);
 });
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
+
 app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
     currency: "INR",
   };
-  stripe.charges.create(body,(stripeErr,stripeRes)=>{
-      if(stripeErr){
-          res.status(500).send({error:stripeErr});
-      }
-      else{
-        res.status(200).send({success:stripeRes});
-      }
-  })
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
